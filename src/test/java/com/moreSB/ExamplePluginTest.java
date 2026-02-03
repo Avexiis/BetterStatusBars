da439524188824506fe4fe623b488108c711f13e
Avexiis <samelykimmel@gmail.com>
2026-02-03 00:13:30 -0500

    Rename to MoreStatusBars

---
+++ src/test/java/com/moreSB/ExamplePluginTest.java
@@ -0,0 +1,13 @@
+package com.moreSB;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class ExamplePluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(MoreStatusBarsPlugin.class);
+		RuneLite.main(args);
+	}
+}
